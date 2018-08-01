910c12c1d638fc68f2bc312b3d83a13ef22833f7
Jan Lindblom <jan.lindblom@kodarklubben.ax>
2018-08-01 08:27:51 +0300

    Some tests and more math.

--- lib/quaternion.rb
+++ lib/quaternion.rb
@@ -11,17 +11,27 @@ class Quaternion
     self.b = 0
     self.c = 0
     self.d = 0
-    unless a.nil? || b.nil? || c.nil? || d.nil?
-      self.a = a
-      self.b = b
-      self.c = c
-      self.d = d
-    end
+    self.a = a unless a.nil?
+    self.b = b unless b.nil?
+    self.c = c unless c.nil?
+    self.d = d unless d.nil?
   end
 
   def to_s
     return '0' if zero?
     "#{format_number a, false} + #{format_number b}i + #{format_number c}j + #{format_number d}k"
+  end
+
+  def conjugate
+    Quaternion.new(a, -1 * b, -1 * c, -1 * d)
+  end
+
+  def norm
+    Math.sqrt(a**2 + b**2 + c**2 + d**2)
+  end
+
+  def versor
+    self / norm
   end
 
   def +(other)
@@ -36,16 +46,16 @@ class Quaternion
   def *(other)
     r = Quaternion.new
     if other.is_a? Quaternion
-      a1 = self.a
+      a1 = a
       a2 = other.a
-      b1 = self.b
+      b1 = b
       b2 = other.b
-      c1 = self.c
+      c1 = c
       c2 = other.c
-      d1 = self.d
+      d1 = d
       d2 = other.d
-      r.a = (a1 * a2 + b1 * b2 + c1 * c2 + d1 * d2)
-      r.b = (a1 * b2 + b1 * a2 + c1 * d2 -d1 * c2)
+      r.a = (a1 * a2 - b1 * b2 - c1 * c2 - d1 * d2)
+      r.b = (a1 * b2 + b1 * a2 + c1 * d2 - d1 * c2)
       r.c = (a1 * c2 - b1 * d2 + c1 * a2 + d1 * b2)
       r.d = (a1 * d2 + b1 * c2 - c1 * b2 + d1 * a2)
     elsif other.is_a? Numeric
@@ -58,8 +68,46 @@ class Quaternion
     r
   end
 
+  def /(other)
+    r = Quaternion.new
+    if other.real?
+      r = self * (1 / other.a)
+    elsif other.is_a? Numeric
+      r = self * (1 / other)
+    end
+    r
+  end
+
+  def eql?(other)
+    return true if a == other.a && b == other.b && c == other.c && d == other.d
+    false
+  end
+
+  def equal?(other)
+    eql?(other)
+  end
+
+  def ==(other)
+    eql?(other)
+  end
+
+  def identity_element?
+    return true if a == 1 && b == 0 && c == 0 && d == 0
+    false
+  end
+
   def zero?
     return true if a == 0 && b == 0 && c == 0 && d == 0
+    false
+  end
+
+  def real?
+    return true if b == 0 && c == 0 && d == 0
+    false
+  end
+
+  def imaginary?
+    return true if a == 0 && (b != 0 || c != 0 || d != 0)
     false
   end
 
@@ -69,7 +117,7 @@ class Quaternion
 
   private
 
-  def format_number(number, omit=true)
+  def format_number(number, omit = true)
     return '' if omit && (number == 1 || number == 1.0)
     number.to_s
   end
--- spec/quaternion_spec.rb
+++ spec/quaternion_spec.rb
@@ -2,4 +2,15 @@ RSpec.describe Quaternion do
   it "has a version number" do
     expect(Quaternion::VERSION).not_to be nil
   end
+
+  it "the real quaternion 1 is the identity element" do
+    q = Quaternion.new(1,0,0,0)
+    expect(q.identity_element?).to be true
+  end
+  it "the real quaternion commute with all other quaternions" do
+    q = Quaternion.new(1,2,3,4)
+    a = Quaternion.new(4,0,0,0)
+    expect(a.real?).to be true
+    expect(a * q).to eq (q * a)
+  end
 end
